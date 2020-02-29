82f33e8ed97f5bdd3ea5269be26f51f088ba93bb
gbroiles <gbroiles@gmail.com>
2020-02-29 14:34:07 -0800

    add print debugs

--- he2.py
+++ he2.py
@@ -22,9 +22,13 @@ def Many_Hash(filename):
     with open(filename, "rb") as f:
         data = f.read()
     md5 = hashlib.md5(data).hexdigest()
+    print("MD5: {}".format(md5)) 
     sha1 = hashlib.sha1(data).hexdigest()
+    print("SHA1: {}".format(sha1)) 
     sha256 = hashlib.sha256(data).hexdigest()
+    print("SHA256: {}".format(sha256))
     sha512 = hashlib.sha512(data).hexdigest()
+    print("SHA512: {}".format(sha512)) 
     return len(data), md5, sha1, sha256, sha512
 
 
