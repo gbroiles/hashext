201cf194d69ccf011441f33f1f9b881d82559a8c
gbroiles <gbroiles@gmail.com>
2020-03-01 18:55:08 -0800

    improve formatting w/ black

--- he2.py
+++ he2.py
@@ -20,17 +20,17 @@ def Many_Hash(filename):
     """ calculate hashes for given filename """
     print("Hashing {}".format(filename))
     with open(filename, "rb") as f:
-#    with open(filename) as f:
+        #    with open(filename) as f:
         data = f.read()
-#    data = data.encode("UTF-8")
+    #    data = data.encode("UTF-8")
     md5 = hashlib.md5(data).hexdigest()
-#    print("MD5: {}".format(md5)) 
+    #    print("MD5: {}".format(md5))
     sha1 = hashlib.sha1(data).hexdigest()
-#    print("SHA1: {}".format(sha1)) 
+    #    print("SHA1: {}".format(sha1))
     sha256 = hashlib.sha256(data).hexdigest()
-#    print("SHA256: {}".format(sha256))
+    #    print("SHA256: {}".format(sha256))
     sha512 = hashlib.sha512(data).hexdigest()
-#    print("SHA512: {}".format(sha512)) 
+    #    print("SHA512: {}".format(sha512))
     return len(data), md5, sha1, sha256, sha512
 
 
--- mh.py
+++ mh.py
@@ -1,33 +1,23 @@
 #! /usr/bin/env python3
 import argparse
-import datetime
-import os
-import random
-import sys
 import hashlib
+
 
 def create_parse():
     """ set up parser options """
-    parser = argparse.ArgumentParser(description="file hashing shell extension")
-    parser.add_argument("target", help="file to be hashed")
+    parser = argparse.ArgumentParser(description="file hashing utility")
+    parser.add_argument("target", help="one or more files to be hashed", nargs="+")
     return parser
 
 
 def Many_Hash(filename):
     """ calculate hashes for given filename """
-    print("Hashing {}".format(filename))
     with open(filename, "rb") as f:
-#    with open(filename) as f:
         data = f.read()
-#    data = data.encode("UTF-8")
     md5 = hashlib.md5(data).hexdigest()
-#    print("MD5: {}".format(md5)) 
     sha1 = hashlib.sha1(data).hexdigest()
-#    print("SHA1: {}".format(sha1)) 
     sha256 = hashlib.sha256(data).hexdigest()
-#    print("SHA256: {}".format(sha256))
     sha512 = hashlib.sha512(data).hexdigest()
-#    print("SHA512: {}".format(sha512)) 
     return len(data), md5, sha1, sha256, sha512
 
 
@@ -35,9 +25,15 @@ def main():
     """ main event loop """
     parser = create_parse()
     args = parser.parse_args()
-    target = args.target
-    size, md5, sha1, sha256, sha512 = Many_Hash(target)
-    print("MD5: {}\nSHA1: {}\nSHA256: {}\nSHA512: {}".format(md5, sha1, sha256, sha512))
+    targets = args.target
+    for target in targets:
+        size, md5, sha1, sha256, sha512 = Many_Hash(target)
+        print(
+            "Name: {}\nSize: {:,}\nMD5: {}\nSHA1: {}\nSHA256: {}\nSHA512: {}\n".format(
+                target, size, md5, sha1, sha256, sha512
+            )
+        )
+
 
 if __name__ == "__main__":
     main()
